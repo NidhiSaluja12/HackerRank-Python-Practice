5c592cb702317c4461bbd19d2d8acfa8cf376eaa
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-23 18:46:05 +0530

    Create SubString_in_String.py

---
+++ SubString_in_String.py
@@ -0,0 +1,10 @@
+def count_substring(string, sub_string):
+    ss_count = 0
+    s_len = len(string)
+    ss_len = len(sub_string)
+
+    for i in range(s_len - 1):
+        if string[i:i+ss_len] == sub_string:
+            ss_count += 1
+
+    return ss_count
