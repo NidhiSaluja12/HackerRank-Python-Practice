f57d6611c6a699c2697118118461774e9c78f18e
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-25 11:38:47 +0530

    Create List_Comprehension.py

---
+++ List_Comprehension.py
@@ -0,0 +1,18 @@
+def List_Comprehension():
+    ls = []
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range (z+1):
+                if i+j+k != n:
+                    ls.append([i, j, k])
+    return ls
+    
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    result = List_Comprehension()
+    print(result)
