60046d675799ef452a1f7bb33e59bdfae197e4d5
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-26 22:22:08 +0530

    Create Set(SymmetricDifference).py

---
+++ Set(SymmetricDifference).py
@@ -0,0 +1,10 @@
+n1 = int(input())
+array1 = list(map(int, input().split()))
+n2 = int(input())
+array2 = list(map(int, input().split()))
+set1 = set(array1)
+set2 = set(array2)
+symm_difference = set1.symmetric_difference(set2)
+result = list(symm_difference)
+print(len(result))
+
