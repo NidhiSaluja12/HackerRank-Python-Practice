2f306397dca966737880a09db4c7e4b3ebc4611a
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-23 22:22:34 +0530

    Update Array_Left_Rotation.py

--- Array_Left_Rotation.py
+++ Array_Left_Rotation.py
@@ -1,19 +1,9 @@
-
-
 import math
 import os
 import random
 import re
 import sys
 
-
-
-
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts following parameters:
-#  1. INTEGER d
-#  2. INTEGER_ARRAY arr
-#
 
 def rotateLeft(d, arr):
     
