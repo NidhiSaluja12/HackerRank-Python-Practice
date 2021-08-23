1672a894f0b1519aad1db5ae587875d71d0d313b
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-23 22:23:07 +0530

    Update Check strict subset.py

--- Check strict subset.py
+++ Check strict subset.py
@@ -1,10 +1,13 @@
 A = set(list(map(int, input().split())))
 n = int(input())
+
 count = 0
+
 for i in range(n):
     B = set(list(map(int, input().split())))
     if A.issuperset(B):
         count+=1
+        
 if count==n:
     print(True)
 else:
