712c63c4ef6b8be6ad209c160768e28b64a67088
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-23 22:23:35 +0530

    Update Check_Subset.py

--- Check_Subset.py
+++ Check_Subset.py
@@ -1,10 +1,12 @@
 T = int(input())        #Test Case
+
 for i in range(T):
     N1 = int(input())
     A = set(list(map(int, input().split())))
     N2 = int(input())
     B = set(list(map(int, input().split())))
     set1 = A.difference(B)
+    
     if len(set1)==0:
         print("True")
     else:
