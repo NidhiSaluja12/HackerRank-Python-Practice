ae5caf7f45d65df39b5845e5313432c14c55e1eb
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-30 22:10:16 +0530

    Update Insertion_sort II.py

--- Insertion_sort II.py
+++ Insertion_sort II.py
@@ -1,18 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'insertionSort2' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY arr
-#
 
 def insertionSort2(n, arr):
     for i in range(1, n):
@@ -22,9 +12,8 @@ def insertionSort2(n, arr):
             arr[pos+1] = arr[pos]
             pos = pos-1
         arr[pos+1] = current_index
+        
         print(*arr)
-            
-    # Write your code here
 
 if __name__ == '__main__':
     n = int(input().strip())
